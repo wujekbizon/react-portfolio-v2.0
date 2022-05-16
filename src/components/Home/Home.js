5c55a615ffc26e95a030afc7f44e75f7653c6dc2
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-05-16 21:34:14 +0200

    start work on landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,19 @@
 import './Home.scss';
+import Logo from '../../assets/images/logo_sub-v3.png';
 
 const Home = () => {
-  return <div>Hi There</div>;
+  const nameArray = ['I', "'", 'm', ' ', 'G', 'r', 'e', 'g', ' '];
+  return (
+    <div>
+      <div className="title">
+        <h1>Welcome to my Portfolio!</h1>
+      </div>
+      <div className="name">
+        {nameArray}
+        <img className="logo" src={Logo} alt="wolfinger" />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
