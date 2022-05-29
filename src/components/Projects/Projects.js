1b484347fcd8a2be5be113c7719ad394986ea631
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-05-29 07:44:09 +0200

    add new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,13 +3,15 @@ import MtgCounter from './MtgCounter/MtgCounter';
 import MovieFight from './MovieFight/MovieFight';
 import YelpCamp from './YelpCamp/YelpCamp';
 import Portfolio from './Portfolio/Portfolio';
+import PortfolioV2 from './PortfolioV2/PortfolioV2';
+
 const Projects = () => {
   return (
     <div>
       <div className="projects-page">
         <h1>MY PROJECTS :</h1>
       </div>
-
+      <PortfolioV2 />
       <MtgCounter />
       <YelpCamp />
       <MovieFight />
