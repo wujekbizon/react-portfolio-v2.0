43417e9c889a1219ce13c652b90eb011325348b6
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-08-13 10:15:27 +0200

    add section skills to  about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,6 +6,14 @@ import Navbar from '../../components/Navbar/Navbar';
 import AI from '../../assets/images/ai.png';
 import Highlight from '../../components/Highlight/Highlight';
 import Brand from '../../components/Brand/Brand';
+import {
+  faNodeJs,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
   return (
@@ -51,32 +59,73 @@ const About = () => {
         </section>
         <Brand />
         <section className="about-bottom">
-          <div className="tech-stack">
-            <h1 className="gradient__text">Tech Stack</h1>
-          </div>
-
-          <div className="right">
-            <div className="skills">
-              <div className="flex">
-                {fontsAwesomeIcons.map((skill) => {
-                  const { id, className, icon, color } = skill;
-                  return (
-                    <div className="item" key={id}>
-                      <p className={className}>
-                        <FontAwesomeIcon icon={icon} color={color} />
-                      </p>
-                    </div>
-                  );
-                })}
-                {reactIcons.map((item) => {
-                  const { id, className, icon } = item;
-                  return (
-                    <div className="item" key={id}>
-                      <p className={className}>{icon}</p>
-                    </div>
-                  );
-                })}
+          <div></div>
+          <div className="cube-container">
+            <div className="cubespinner">
+              <div className="face1 gradient__bg">
+                <FontAwesomeIcon icon={faNodeJs} />
               </div>
+              <div className="face2 gradient__bg">
+                <FontAwesomeIcon icon={faHtml5} />
+              </div>
+              <div className="face3 gradient__bg">
+                <FontAwesomeIcon icon={faCss3} />
+              </div>
+              <div className="face4 gradient__bg">
+                <FontAwesomeIcon icon={faReact} />
+              </div>
+              <div className="face5 gradient__bg">
+                <FontAwesomeIcon icon={faGitAlt} />
+              </div>
+              <div className="face6 gradient__bg2">
+                <FontAwesomeIcon icon={faJsSquare} />
+              </div>
+            </div>
+          </div>
+          <div className="tech-stack">
+            {/* <div>🛠</div> */}
+            <h1 className="gradient__text">Tech Stack</h1>
+            <p>
+              To build a modern , fully responsive websites , you need a set off
+              skills.{' '}
+            </p>
+          </div>
+        </section>
+
+        <section className="skills-container section__margin">
+          <div className="skills gradient__container">
+            <div className="skills-content">
+              <div className="title">
+                <div className="gradient__text" />
+                <h1>Skills I use as a programmer</h1>
+              </div>
+
+              <p>
+                Those are the skills, that I acquired during the process of my
+                everyday learning. Some of them I know good, some of them good
+                enough. I understand that to be a good programmer, you need to
+                learn and continue develop new skills.
+              </p>
+            </div>
+            <div className="flex">
+              {fontsAwesomeIcons.map((skill) => {
+                const { id, className, icon, color } = skill;
+                return (
+                  <div className="item" key={id}>
+                    <p className={className}>
+                      <FontAwesomeIcon icon={icon} color={color} />
+                    </p>
+                  </div>
+                );
+              })}
+              {reactIcons.map((item) => {
+                const { id, className, icon } = item;
+                return (
+                  <div className="item" key={id}>
+                    <p className={className}>{icon}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </section>
